a6273947b1ee1f746695fe9b30b07346810d3e95
NazarovIlya <eliakohai@gmail.com>
2023-08-22 09:08:08 +0300

    server redy, it is necessary to rewrite the client's methods

---
+++ backend/src/main/java/tech/contacts/contactapp/env/Settings.java
@@ -0,0 +1,10 @@
+package tech.contacts.contactapp.env;
+
+final public class Settings {
+  private static String HOST = "http://localhost:3000";
+
+  public static String getHOST() {
+    return HOST;
+  }
+
+}
