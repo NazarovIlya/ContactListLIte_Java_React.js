1bc3035690d5c376f55fb274426ba15c36fb6311
NazarovIlya <eliakohai@gmail.com>
2024-02-07 23:50:17 +0300

    updated

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,6 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App url='http://192.168.56.1:8080/api'/>
+    <App url='http://localhost:8080/api'/>
   </React.StrictMode>
 );
